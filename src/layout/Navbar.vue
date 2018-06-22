b6960f0392237a14a3835df21ce8e7a5fd153516
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-06-22 20:12:09 +0200

    logging a user out

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -3,14 +3,26 @@
     nav
       router-link(:to="{ name: 'Signup' }") Signup
       router-link(:to="{ name: '' }") Login
+      a(@click="logout") Logout
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'Navbar',
   data() {
     return {
 
+    }
+  },
+  methods: {
+    logout() {
+      firebase.auth().signOut()
+      .then(() => {
+        this.$router.push({ name: 'Signup' })
+      })
+      .catch(error => { console.log(error) })
     }
   }
 }
